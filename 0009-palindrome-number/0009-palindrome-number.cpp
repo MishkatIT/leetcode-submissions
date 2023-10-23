6b259058de8aa45efa3a8f20263f2eb4b551f89e
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2023-10-23 23:59:40 +0600

    approach Update 0009-palindrome-number.cpp

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -10,3 +10,24 @@ public:
         return ok;
     }
 };
+
+/*
+    Approach:
+    just iterate from the beginning to n / 2 
+    and check if the first character is the same as the last character, 
+    the second character is the same as the second last character, and so on.
+
+    Don't bother with the odd length of the string :)
+    Look at this example: 
+    abcba
+    length n = 5, so n / 2 = 2 (rounding down from 2.5)
+    the loop goes like this,
+     (considering 0-based indexing)
+    is str[0] == str[4] , yes
+    is str[1] == str[3], yes
+    loop breaks. 
+
+    you don't need to check the middle one, that is str[2];
+    if it is checked then no problem by the way, then it'd be str[2] == str[2] it's comparing with itself :)
+    
+*/
