8dfe253d2387c48e090cb2c468b16607760c3f37
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-06-07 16:58:27 +0600

    Time: 681 ms (14.76%), Space: 511.5 MB (5.57%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,12 +1,64 @@
+struct Node {
+    Node* links[26];
+    bool flag = false;
+    bool containsKey(char ch) {
+        return links[ch - 'a'] != nullptr;
+    }
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+    void setEnd() {
+        flag = true;
+    }
+    bool isEnd() {
+        return flag;
+    }
+};
+
+class Trie {
+public:
+    Node* root;
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node* node = root;
+        for (char i : word) {
+            if (!node->containsKey(i)) {
+                node->put(i, new Node());
+            }
+            node = node->get(i); // move to the reference trie
+        }
+        node->setEnd();
+    }
+
+    bool search(string word) {
+        Node* node = root;
+        for (char i : word) {
+            if (!node->containsKey(i)) {
+                return false;
+            }
+            node = node->get(i);
+        }
+        return node->isEnd();
+    }
+
+
+};
+
+
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
 
-        map<string, bool> mp;
+        Trie tr;
         for (auto& i : dictionary) {
-            mp[i] = true;
+            tr.insert(i);
         }
-
         stringstream ss(sentence);
         string str;
         string ans;
@@ -14,7 +66,7 @@ public:
             string temp;
             for (auto& i : str) {
                 temp += i;
-                if (mp[temp]) {
+                if (tr.search(temp)) {
                     break;
                 }
             }
