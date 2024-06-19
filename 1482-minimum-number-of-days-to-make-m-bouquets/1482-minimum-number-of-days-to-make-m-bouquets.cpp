63edca82463696deadf2e16cec2d5692dcc3bc7f
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-06-20 02:28:27 +0600

    Time: 669 ms (5.07%), Space: 103.1 MB (12.67%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -11,7 +11,7 @@ public:
             deque<int> dq;
             int last = 0;
             for (int i = 0; i < n; i++) {
-                while (!dq.empty() && bloomDay[dq.back()] < bloomDay[i]) dq.pop_back();
+                while (!dq.empty() && bloomDay[dq.back()] <= bloomDay[i]) dq.pop_back();
                 dq.push_back(i);
                 if (dq.front() <= i - k) dq.pop_front();
                 if (dq.back() - last + 1 >= k) {
