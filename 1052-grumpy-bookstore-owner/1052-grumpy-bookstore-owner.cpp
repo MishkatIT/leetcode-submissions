d1b32e22802b5458d3925d1debd110ffece9849d
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-06-21 19:08:12 +0600

    Time: 80 ms (9.38%), Space: 35.6 MB (5.44%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -4,7 +4,7 @@ public:
         int n = customers.size();
         vector<int> original(n + 1), pref(n + 1);
         for (int i = 1; i <= n; i++) {
-            original[i] = original[i - 1] + customers[i - 1] * grumpy[i - 1];
+            original[i] = original[i - 1] + customers[i - 1] * !grumpy[i - 1];
         }
         for (int i = 1; i <= n; i++) {
             pref[i] = pref[i - 1] + customers[i - 1];
@@ -13,6 +13,10 @@ public:
         for (int i = minutes; i <= n; i++) {
             ans = max(ans, pref[i] - pref[i - minutes] + original[n] - (original[i] - original[i - minutes]));
         }
+        for (int i = 1; i <= n; i++) cout << original[i] << ' ';
+        cout << endl;
+        for (int i = 1; i <= n; i++) cout << pref[i] << ' ';
+        cout << endl;
         return ans;
     }
 };
