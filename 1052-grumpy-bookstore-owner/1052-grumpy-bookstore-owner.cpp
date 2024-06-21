76d856753853905d5c8a7816bdbb8d2fe31601f4
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-06-21 19:08:00 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int n = customers.size();
+        vector<int> original(n + 1), pref(n + 1);
+        for (int i = 1; i <= n; i++) {
+            original[i] = original[i - 1] + customers[i - 1] * grumpy[i - 1];
+        }
+        for (int i = 1; i <= n; i++) {
+            pref[i] = pref[i - 1] + customers[i - 1];
+        }
+        int ans = 0;
+        for (int i = minutes; i <= n; i++) {
+            ans = max(ans, pref[i] - pref[i - minutes] + original[n] - (original[i] - original[i - minutes]));
+        }
+        return ans;
+    }
+};
