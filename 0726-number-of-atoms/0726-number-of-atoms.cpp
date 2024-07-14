729c6cb7c7448769a8b547b6aba6fb4d6438198b
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-07-14 23:45:25 +0600

    Time: 7 ms (9.24%), Space: 9.4 MB (49.79%) - LeetHub

---
+++ 0726-number-of-atoms/0726-number-of-atoms.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    string countOfAtoms(string formula) {
+        map<string, int> mp;
+        int pos = 0;
+        mp = func(formula, pos);
+        string ans;
+        for (auto& i : mp) {
+            ans += i.first;
+            if (i.second != 1) {
+                ans += to_string(i.second);
+            }
+        }
+        return ans;
+    }
+
+        map<string, int> func (string &str, int& i) {
+        map<string, int> mp;
+        string temp;
+        int n = str.size();
+        while (i < n) {
+            if (str[i] == ')') {
+                i++;
+                int dig = 0;
+                while (i < n && str[i] >= '0' && str[i] <= '9') {
+                    dig *= 10;
+                    dig += (str[i] - '0');
+                    i++;
+                }
+                for (auto& s: mp) {
+                    s.second *= max(1, dig);
+                }
+                return mp;
+            }
+            if (i < n && str[i] >= '0' && str[i] <= '9') {
+                int dig = 0;
+                while (i < n && str[i] >= '0' && str[i] <= '9') {
+                    dig *= 10;
+                    dig += (str[i] - '0');
+                    i++;
+                }
+                mp[temp] += max(1, dig) - 1;
+            }
+
+            if (i < n && str[i] == '(') {
+                i++;
+                map<string, int> zz = func(str, i);
+                for (auto& s : zz) {
+                    mp[s.first] += s.second;
+                }
+            }
+
+            if (i < n && str[i] >= 'A' && str[i] <= 'Z') {
+                temp.clear();
+                temp += str[i++];
+                while (i < n && str[i] >= 'a' && str[i] <= 'z') {
+                    temp += str[i];
+                    i++;
+                }
+                cout << temp << ' ';
+                mp[temp]++;
+            }
+        }
+        return mp;
+    }
+};
