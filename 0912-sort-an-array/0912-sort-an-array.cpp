96329eec12dacdf37bb30c3ead7483c402dad41c
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-07-25 19:02:06 +0600

    Time: 108 ms (74.36%), Space: 71.4 MB (60.44%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        return srt(nums, 0, (int)nums.size() - 1);
+        vector<int> temp(nums.size());
+        srt(nums, temp, 0, nums.size() - 1);
+        return nums;
     }
 
-    vector<int> srt(vector<int>& v, int s, int e) {
-        if (s == e) return {v[s]};
-        int mid = (s + e) / 2;
-        vector<int> left = srt(v, s, mid);
-        vector<int> right = srt(v, mid + 1, e);
-        vector<int> temp;
-        int i = 0, j = 0;
-        while (i < (int)left.size() && j < (int)right.size()) {
-            if (left[i] <= right[j]) {
-                temp.push_back(left[i++]);
+private:
+    void srt(vector<int>& v, vector<int>& temp, int s, int e) {
+        if (s >= e) return;
+        int mid = s + (e - s) / 2;
+        srt(v, temp, s, mid);
+        srt(v, temp, mid + 1, e);
+        merge(v, temp, s, mid, e);
+    }
+
+    void merge(vector<int>& v, vector<int>& temp, int s, int mid, int e) {
+        int i = s, j = mid + 1, k = s;
+        while (i <= mid && j <= e) {
+            if (v[i] <= v[j]) {
+                temp[k++] = v[i++];
             } else {
-                temp.push_back(right[j++]);
+                temp[k++] = v[j++];
             }
         }
-        while (i < (int)left.size()) {
-            temp.push_back(left[i++]);
+        while (i <= mid) {
+            temp[k++] = v[i++];
         }
-        while (j < (int)right.size()) {
-            temp.push_back(right[j++]);
+        while (j <= e) {
+            temp[k++] = v[j++];
         }
-        return temp;
+        for (int l = s; l <= e; ++l) {
+            v[l] = temp[l];
+        }
     }
 };
