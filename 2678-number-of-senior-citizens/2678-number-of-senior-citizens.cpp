77bcde3506264932eaa46f468c5151707e10a7f2
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-08-01 21:00:18 +0600

    Time: 8 ms (47.01%), Space: 17.6 MB (16.71%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int countSeniors(vector<string>& details) {
         int cnt = 0;
-        for (auto& i : details) {
+        for (auto& dig : details) {
             string x;
-            x += i[11];
-            x += i[12];
+            x += dig[11];
+            x += dig[12];
             cnt += (stoi(x) > 60);
         }
         return cnt;
