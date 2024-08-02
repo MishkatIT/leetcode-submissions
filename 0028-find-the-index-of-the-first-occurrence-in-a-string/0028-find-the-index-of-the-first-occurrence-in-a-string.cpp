a4cd07d37a476d1af03082f269bc8e4d1e9878e0
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-08-02 16:22:38 +0600

    Time: 0 ms (100%), Space: 8 MB (23.93%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,11 +1,47 @@
+
+void computeLPS(int m, string& ptr, vector<int>& lps) {
+    int len = 0, i = 1;
+    while (i < m) {
+        if (ptr[i] == ptr[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+}
+
+
+int KMP(string& str, string& ptr) {
+    int n = str.length(), m = ptr.length();
+    vector<int> lps(m);
+    computeLPS(m, ptr, lps);
+    int i = 0, j = 0;
+    while (i < n) {
+        if (str[i] == ptr[j]) {
+            i++, j++;
+        } else {
+            if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+        if (j == m) {
+            return (i - j);
+        }
+    }
+    return -1;
+}
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        auto pos = haystack.find(needle);
-        if(pos != string::npos) {
-            return pos;
-        } else {
-            return -1;
-        }
+        return KMP(haystack, needle);
     }
 };
