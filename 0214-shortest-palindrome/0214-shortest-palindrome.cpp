b229f3bc226200058c3bba59bb0cb0548b6cf9b1
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-09-21 12:25:31 -0700

    Time: 14 ms (12.6%), Space: 13 MB (6.5%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,51 +1,54 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        if (s.empty()) return s;
+        if (s.empty())
+            return s;
+
+        // Find the longest palindromic prefix
         int mxLen = longestPalindromeStartingFromZero(s);
         if (mxLen == s.size() - 1) return s;
-        if (mxLen == 0) {
-            string temp = s;
-            temp.erase(temp.begin());
-            reverse(temp.begin(), temp.end());
-            return temp + s;
-        }
+
+        // Create the reversed part
         string temp = s.substr(mxLen + 1);
         reverse(temp.begin(), temp.end());
         return temp + s;
     }
-int longestPalindromeStartingFromZero(const string& str) {
-    string s = "^#"; 
-    for (char c : str) {
-        s += c;
-        s += '#'; 
+
+    int longestPalindromeStartingFromZero(const string& str) {
+        string t = "#";
+        for (auto& i : str) {
+            t += i;
+            t += "#";
+        }
+
+        int n = t.size();
+        vector<int> p(n, 0);
+        int center = 0, right = 0, maxLen = 0;
+
+        for (int i = 0; i < n; ++i) {
+            int mirror = 2 * center - i;
+            
+            // If i is within the right boundary, use the mirror value
+            if (i < right) {
+                p[i] = min(p[mirror], right - i);
+            }
+
+            // Expand around center i
+            while (i + p[i] + 1 < n && i - p[i] - 1 >= 0 && t[i + p[i] + 1] == t[i - p[i] - 1]) {
+                ++p[i];
+            }
+
+            // Update the right boundary and center
+            if (i + p[i] > right) {
+                right = i + p[i];
+                center = i;
+            }
+
+            // Check if the palindrome starts at index 0
+            if (i - p[i] == 0) {
+                maxLen = max(maxLen, p[i]);
+            }
+        }
+        return maxLen - 1;
     }
-    s += '$'; 
-
-    int n = s.size();
-    vector<int> p(n, 0);
-    int center = 0, right = 0;
-
-    for (int i = 1; i < n - 1; ++i) {
-        if (i < right) {
-            p[i] = min(right - i, p[2 * center - i]);
-        }
-        while (s[i + p[i] + 1] == s[i - p[i] - 1]) {
-            ++p[i];
-        }
-        if (i + p[i] > right) {
-            center = i;
-            right = i + p[i];
-        }
-    }
-
-    int maxLength = 0;
-    for (int i = 1; i < n - 1; ++i) {
-        if (i - p[i] == 1) { 
-            maxLength = max(maxLength, p[i]);
-        }
-    }
-
-    return maxLength - 1;
-}
 };
