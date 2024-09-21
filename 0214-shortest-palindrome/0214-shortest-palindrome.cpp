e5d613fd248756855a6185915a418e4be52d39ed
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-09-21 12:31:02 -0700

    Time: 13 ms (13.38%), Space: 12.9 MB (6.76%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -3,18 +3,61 @@ public:
     string shortestPalindrome(string s) {
         if (s.empty())
             return s;
-
-        // Find the longest palindromic prefix
         int mxLen = longestPalindromeStartingFromZero(s);
         if (mxLen == s.size() - 1) return s;
-
-        // Create the reversed part
+        if (mxLen == 0) {
+            string temp = s;
+            temp.erase(temp.begin());
+            reverse(temp.begin(), temp.end());
+            return temp + s;
+        }
         string temp = s.substr(mxLen + 1);
         reverse(temp.begin(), temp.end());
         return temp + s;
     }
 
-    int longestPalindromeStartingFromZero(const string& str) {
+
+
+
+
+
+    int ongestPalindromeStartingFromZero(const string& str) {
+        int mx = 0;
+        string t = "#";
+        for (auto& i : str) {
+            t += i;
+            t += "#";
+        }
+        int n = t.size();
+        vector<int> p(n);
+        int center = 0, right = 0;
+        for (int i = 0; i < n; i++) {
+            int mirror = 2 * center - right;
+            if (i < right) {
+                p[i] = min(p[mirror], right - i);
+            }
+            while (i + p[i] + 1 < n && i - p[i] - 1 >= 0 && t[i + p[i] + 1] == t[i - p[i] - 1]) {
+                p[i]++;
+            }
+            if (i + p[i] > right) {
+                right = i + p[i];
+                center = i;
+            }
+             if (p[i] - i == 0) {
+                mx = p[i];
+            }
+        }
+        
+        return mx - 1;
+    }
+
+
+
+
+
+
+
+  int longestPalindromeStartingFromZero(const string& str) {
         string t = "#";
         for (auto& i : str) {
             t += i;
@@ -51,4 +94,9 @@ public:
         }
         return maxLen - 1;
     }
+
+
+
+
+
 };
