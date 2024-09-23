61ed0e417c0fc85e2ac526655726407a19541897
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-09-23 11:50:24 -0700

    Time: 47 ms (96.79%), Space: 111.6 MB (28.38%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,11 +1,48 @@
+class trieNode{
+public:
+    bool isEnd;
+    trieNode* child[26];
+    trieNode() {
+        isEnd = false;
+        for (int i = 0; i < 26; i++) {
+            child[i] = nullptr;
+        }
+    }
+};
+
+class trie{
+public:
+    trieNode* root;
+    trie() {
+        root = new trieNode();
+    }
+
+    int getIdx(char c) {
+        return c - 'a';
+    }
+
+    void insert(const string& str) {
+        trieNode* node = root;
+        for (auto& c : str) {
+            int idx = getIdx(c);
+            if (!node->child[idx]) {
+                node->child[idx] = new trieNode();
+            }
+            node = node->child[idx];
+        }
+        node->isEnd = true;
+    }
+};
+
+
 class Solution {
 public:
-    map<string, bool> mp;
+    trie tr;
     vector<int> dp;
     int minExtraChar(string s, vector<string>& dictionary) {
         int n = s.size();
         for (auto& i : dictionary) {
-            mp[i] = true;
+            tr.insert(i);
         }
         dp.assign(n + 5, -1);
         return solve(0, s); 
@@ -14,10 +51,11 @@ public:
           if (i == str.size()) return 0;
           if (dp[i] != -1) return dp[i];
           int ans = 1 + solve(i + 1, str);
-          string temp;
+          trieNode* node = tr.root;
           for (int j = i; j < str.size(); j++) {
-            temp += str[j];
-            if (mp[temp]) {
+            if (!node->child[str[j] - 'a']) break;
+            node = node->child[str[j] - 'a']; 
+            if (node->isEnd) {
                 ans = min(ans, solve(j + 1, str));
             }
           }
