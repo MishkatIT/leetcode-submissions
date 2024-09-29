21f737cfb9df1dd25015741aaebdb23d54b141e1
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-09-29 13:25:45 -0700

    Time: 108 ms (27.22%), Space: 56.4 MB (63.9%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -1,39 +1,48 @@
 class AllOne {
 public:
-    AllOne() {}
-    
+    AllOne() {
+        
+    }
+    map<string, int> freq;
+    map<int, set<string>> maxmin;
     void inc(string key) {
-        int count = countMap[key]++;
-        if (count > 0) {
-            countSet[count].erase(key);
-            if (countSet[count].empty()) {
-                countSet.erase(count);
+        freq[key]++;
+        int cnt = freq[key];
+        if (cnt > 1) {
+            maxmin[cnt - 1].erase(key);
+            if (maxmin[cnt - 1].size() == 0) {
+                maxmin.erase(cnt - 1);
             }
         }
-        countSet[count + 1].insert(key);
+        maxmin[cnt].insert(key);
     }
     
     void dec(string key) {
-        if (countMap[key] == 0) return;
-        int count = countMap[key]--;
-        countSet[count].erase(key);
-        if (countSet[count].empty()) {
-            countSet.erase(count);
-        }
-        if (count > 1) {
-            countSet[count - 1].insert(key);
+        freq[key]--;
+        int cnt = freq[key];
+        if (cnt != 0) {
+            maxmin[cnt].insert(key);
+        } 
+        maxmin[cnt + 1].erase(key);
+        if (maxmin[cnt + 1].size() == 0) {
+            maxmin.erase(cnt + 1);
         }
     }
     
     string getMaxKey() {
-        return countSet.empty() ? "" : *countSet.rbegin()->second.begin();
+        return (maxmin.empty() ? "" : *maxmin.rbegin()->second.begin());
     }
     
     string getMinKey() {
-        return countSet.empty() ? "" : *countSet.begin()->second.begin();
+        return (maxmin.empty() ? "" : *maxmin.begin()->second.begin());
     }
-
-private:
-    unordered_map<string, int> countMap;
-    map<int, set<string>> countSet;
 };
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
