ef0a9d458cfed90fc674a1d96c456150f56255b5
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-10-04 02:19:57 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        int sum = 0;
+        int n = skill.size();
+        for (auto& i : skill) sum += i;
+        if (sum % (n / 2) != 0) return -1;
+        int avg = sum / (n / 2);
+        long long ans = 0;
+        map<int, int> have;
+        for (auto& i : skill) {
+            if (i >= avg) return -1;
+            if (have[avg - i]) {
+                ans += i * (avg - i);
+                have[avg - i]--;
+            } else {
+                have[i]++;
+            }
+        }
+        return ans;
+    }
+};
