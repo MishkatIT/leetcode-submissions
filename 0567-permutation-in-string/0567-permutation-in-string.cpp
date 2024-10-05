da5ab16f27368061c753d004f65ed344075efaf8
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-10-05 06:38:43 -0700

    Time: 3 ms (94.61%), Space: 8.8 MB (80.36%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s1.size() > s2.size())
-            return false;
-        vector<int> v(26, 0), temp (26, 0);
-        for (int i = 0; i < s1.size(); i++) {
-                v[s1[i] - 'a']++;
-                temp[s2[i] - 'a']++;
-        }   
-        if(v == temp)
-            return true;
-        int n = s2.size() - s1.size();
-        for (int i = 0; i < n; i++) {
-            temp[s2[i] - 'a']--;
-            temp[s2[s1.size() + i] - 'a']++;
-            if(v == temp)
-                return true;    
+        vector<int> fa(26, 0), fb(26, 0);
+        for (auto& i : s1) {
+            fa[i - 'a']++;
         }
-        return false;    
+        for (int i = 0; i < s2.size(); i++) {
+            if (i >= s1.size()) {
+                fb[s2[i - s1.size()] - 'a']--;
+            }
+            fb[s2[i] - 'a']++;
+            if (fa == fb) return true;
+        }
+        return false;
     }
 };
