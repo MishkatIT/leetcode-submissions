66b7b1dd53c448d191503395b7e349dc98fa2e9a
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2024-10-12 12:15:44 -0700

    Time: 424 ms (23.2%), Space: 122.1 MB (32.21%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        vector<int> diff(1e6 + 6);
+        map<int, int> diff;
         for (auto& i : intervals) {
             diff[i[0]]++;
             diff[i[1] + 1]--;
         }
-        for (int i = 1; i < (int)1e6 + 5; i++) {
-            diff[i] += diff[i - 1];
+        int cur = 0;
+        int mx = 0;
+        for (auto& i : diff) {
+            i.second += cur;
+            cur = i.second;
+            mx = max(cur, mx);
         }
-        return *max_element(diff.begin(), diff.end());
+        return mx;
     }
 };
