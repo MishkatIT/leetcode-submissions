479a823a4ee44ce2f9444968bb99f351ae796442
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2025-02-17 04:14:42 -0800

    Time: 0 ms (100%), Space: 7.8 MB (92.23%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int numTilePossibilities(string tiles) {
-        set<string> s;
-        string x;
-        function<void(int)> check = [&](int i) {
-            if (i == tiles.size()) {
-                if (!x.empty()) s.insert(x);
-                return;
+    int cnt = 0;
+    map<char, int> mp;
+    void dfs() { 
+        for (auto& i : mp) {
+            if (i.second > 0) {
+                i.second--;
+                cnt += 1;
+                dfs();
+                i.second++;
             }
-            x += tiles[i];
-            check(i + 1);
-            x.pop_back();
-            check(i + 1);
-        };
+        }
+    }
 
-        sort(tiles.begin(), tiles.end());
-
-        do {
-            x.clear();
-            check(0);
-        } while (next_permutation(tiles.begin(), tiles. end()));
-
-        return s.size();
+    int numTilePossibilities(string tiles) {
+        for (auto& i : tiles) {
+            mp[i]++;
+        }
+        dfs();
+        return cnt;
     }
 };
