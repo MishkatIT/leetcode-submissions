bfa06e86f7f7e61ceb815f40d5c31fcd61a2f903
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2025-10-01 23:31:04 +0600

    Time: 0 ms (100%), Space: 7.9 MB (11.04%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-       return numBottles + func(numBottles, numExchange);
-    }
-
-     int func(int n, int e) {
         int ans = 0;
-        if (n >= e) {
-            ans += n / e;
-        } else return ans;
-        return ans + func(n / e + n % e, e);
-     }
+        for (int i = 1; i <= numBottles; i++) {
+            ans++;
+            if (i % numExchange == 0) numBottles++;
+        }
+        return ans;
+    }
 };
